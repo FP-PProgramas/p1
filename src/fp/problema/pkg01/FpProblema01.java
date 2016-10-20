98f0b92b5e2dadeadd445e665d5f1e8400dfb4ec
carlos <carlos@carlosrgg>
2016-10-20 11:41:30 -0500

    Proyecto parcial

--- src/fp/problema/pkg01/FpProblema01.java
+++ src/fp/problema/pkg01/FpProblema01.java
@@ -3,12 +3,7 @@ package fp.problema.pkg01;
 import java.util.*;
 
 public class FpProblema01 {
-/**
-  Problemas.
-1. Escribir un programa que determine si un año es bisiesto. Una año es bisiesto si el múltiplo de 4.
-* (Ej. 1984. Los años múltiplos de 100 no son bisiestos, salvo si ellos son también múltiplos de 400 
-* (2000 es bisiesto, 1800 no lo es).
-**/
+
     public static void main(String[] args) {
      int a;
      Scanner entradaDato= new Scanner(System.in);
